6018c716dea96834b7d24f62f34577f770ac93f9
muhamadnursyami <nursami385@gmail.com>
2024-08-26 12:42:30 +0700

    style: hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,210 +1,239 @@
+import { useState, useEffect } from "react";
+
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <div className="navbar bg-base-100 container">
-        <div className="navbar-start">
-          <a>
-            <img
-              src="/logo-navbar.png"
-              alt="Logo"
-              className="w-full h-[60px]"
-            />
-          </a>
-        </div>
-        <div className="navbar-end lg:hidden">
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
+      <div
+        className={`fixed top-2 left-0 right-0 font-semibold z-50 flex justify-center transition-all duration-300 `}
+      >
+        <div
+          className={`transition-all py-2 duration-300 ${
+            isScrolled ? "rounded-lg bg-white shadow-lg" : ""
+          } inline-block`}
+        >
+          <div className="container mx-auto flex items-center justify-between px-4 lg:px-8">
+            <div className={`navbar-start`}>
+              <a>
+                <img
+                  src="/logo-navbar.png"
+                  alt="Logo"
+                  className="md:h-[60px] h-auto w-auto"
                 />
-              </svg>
+              </a>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Beranda</a>
-              </li>
-              <li>
-                <details>
-                  <summary>Pemerintahan</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Sejarah</a>
-                    </li>
-                    <li>
-                      <a>Visi dan Misi</a>
-                    </li>
-                    <li>
-                      <a>Perangkat Daerah</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>Layanan Publik</a>
-              </li>
-              <li>
-                <details>
-                  <summary>Publikasi</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Berita</a>
-                    </li>
-                    <li>
-                      <a>Pengumuman</a>
-                    </li>
-                    <li>
-                      <a>Agenda</a>
-                    </li>
-                    <li>
-                      <a>Statistik</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <details>
-                  <summary>Dokumen & Peraturan</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Perda APBD 2024</a>
-                    </li>
-                    <li>
-                      <a>Perda APBD 2023</a>
-                    </li>
-                    <li>
-                      <a>APBD Kepri 2023</a>
-                    </li>
-                    <li>
-                      <a>Opini BPK RI 2023</a>
-                    </li>
-                    <li>
-                      <a>Laporan BPKD</a>
-                    </li>
-                    <li>
-                      <a>Produk Hukum</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <details>
-                  <summary>Galeri</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Foto & Video</a>
-                    </li>
-                    <li>
-                      <a>Aset MultiMedia</a>
-                    </li>
-                    <li>
-                      <a>AI Kepri</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-            </ul>
+            <div className="navbar-end flex items-center lg:hidden">
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="btn btn-ghost">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                >
+                  <li>
+                    <a>Beranda</a>
+                  </li>
+                  <li>
+                    <details>
+                      <summary>Pemerintahan</summary>
+                      <ul className="p-2">
+                        <li>
+                          <a>Sejarah</a>
+                        </li>
+                        <li>
+                          <a>Visi dan Misi</a>
+                        </li>
+                        <li>
+                          <a>Perangkat Daerah</a>
+                        </li>
+                      </ul>
+                    </details>
+                  </li>
+                  <li>
+                    <a>Layanan Publik</a>
+                  </li>
+                  <li>
+                    <details>
+                      <summary>Publikasi</summary>
+                      <ul className="p-2">
+                        <li>
+                          <a>Berita</a>
+                        </li>
+                        <li>
+                          <a>Pengumuman</a>
+                        </li>
+                        <li>
+                          <a>Agenda</a>
+                        </li>
+                        <li>
+                          <a>Statistik</a>
+                        </li>
+                      </ul>
+                    </details>
+                  </li>
+                  <li>
+                    <details>
+                      <summary>Dokumen & Peraturan</summary>
+                      <ul className="p-2">
+                        <li>
+                          <a>Perda APBD 2024</a>
+                        </li>
+                        <li>
+                          <a>Perda APBD 2023</a>
+                        </li>
+                        <li>
+                          <a>APBD Kepri 2023</a>
+                        </li>
+                        <li>
+                          <a>Opini BPK RI 2023</a>
+                        </li>
+                        <li>
+                          <a>Laporan BPKD</a>
+                        </li>
+                        <li>
+                          <a>Produk Hukum</a>
+                        </li>
+                      </ul>
+                    </details>
+                  </li>
+                  <li>
+                    <details>
+                      <summary>Galeri</summary>
+                      <ul className="p-2">
+                        <li>
+                          <a>Foto & Video</a>
+                        </li>
+                        <li>
+                          <a>Aset MultiMedia</a>
+                        </li>
+                        <li>
+                          <a>AI Kepri</a>
+                        </li>
+                      </ul>
+                    </details>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                <li>
+                  <a>Beranda</a>
+                </li>
+                <li>
+                  <details>
+                    <summary>Pemerintahan</summary>
+                    <ul className="p-2">
+                      <li>
+                        <a>Sejarah</a>
+                      </li>
+                      <li>
+                        <a>Visi dan Misi</a>
+                      </li>
+                      <li>
+                        <a>Perangkat Daerah</a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+                <li>
+                  <a>Layanan Publik</a>
+                </li>
+                <li>
+                  <details>
+                    <summary>Publikasi</summary>
+                    <ul className="p-2">
+                      <li>
+                        <a>Berita</a>
+                      </li>
+                      <li>
+                        <a>Pengumuman</a>
+                      </li>
+                      <li>
+                        <a>Agenda</a>
+                      </li>
+                      <li>
+                        <a>Statistik</a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+                <li>
+                  <details>
+                    <summary>Dokumen & Peraturan</summary>
+                    <ul className="p-2">
+                      <li>
+                        <a>Perda APBD 2024</a>
+                      </li>
+                      <li>
+                        <a>Perda APBD 2023</a>
+                      </li>
+                      <li>
+                        <a>APBD Kepri 2023</a>
+                      </li>
+                      <li>
+                        <a>Opini BPK RI 2023</a>
+                      </li>
+                      <li>
+                        <a>Laporan BPKD</a>
+                      </li>
+                      <li>
+                        <a>Produk Hukum</a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+                <li>
+                  <details>
+                    <summary>Galeri</summary>
+                    <ul className="p-2">
+                      <li>
+                        <a>Foto & Video</a>
+                      </li>
+                      <li>
+                        <a>Aset MultiMedia</a>
+                      </li>
+                      <li>
+                        <a>AI Kepri</a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Beranda</a>
-            </li>
-            <li>
-              <details>
-                <summary>Pemerintahan</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Sejarah</a>
-                  </li>
-                  <li>
-                    <a>Visi dan Misi</a>
-                  </li>
-                  <li>
-                    <a>Perangkat Daerah</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Layanan Publik</a>
-            </li>
-            <li>
-              <details>
-                <summary>Publikasi</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Berita</a>
-                  </li>
-                  <li>
-                    <a>Pengumuman</a>
-                  </li>
-                  <li>
-                    <a>Agenda</a>
-                  </li>
-                  <li>
-                    <a>Statistik</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <details>
-                <summary>Dokumen & Peraturan</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Perda APBD 2024</a>
-                  </li>
-                  <li>
-                    <a>Perda APBD 2023</a>
-                  </li>
-                  <li>
-                    <a>APBD Kepri 2023</a>
-                  </li>
-                  <li>
-                    <a>Opini BPK RI 2023</a>
-                  </li>
-                  <li>
-                    <a>Laporan BPKD</a>
-                  </li>
-                  <li>
-                    <a>Produk Hukum</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <details>
-                <summary>Galeri</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Foto & Video</a>
-                  </li>
-                  <li>
-                    <a>Aset MultiMedia</a>
-                  </li>
-                  <li>
-                    <a>AI Kepri</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
-        {/* <div className="navbar-end"><a className="btn">Button</a></div> */}
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <div className="relative">
+        {/* Background Image */}
+        <img
+          src="/bg-kepri.jpg"
+          alt="Kepri"
+          className="w-full h-screen object-cover"
+        />
+
+        {/* Dark and Blur Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-20 backdrop-blur-sm z-0"></div>
+
+        {/* Main Text */}
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <h1 className="text-white text-2xl lg:text-4xl font-bold">
+            Selamat Datang di Portal Kepulauan Riau
+          </h1>
+        </div>
+
+        {/* Wave SVG */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute -bottom-10 left-0 w-full"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#FAFAFA"
+            fillOpacity="1"
+            d="M0,224L120,234.7C240,245,480,267,720,266.7C960,267,1200,245,1320,234.7L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+    </>
   );
 }
